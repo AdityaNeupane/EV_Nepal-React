a6b394532c65eebb76163a3672ec2434b87c19e9
adityaneupane <bijayneupane324@gmail.com>
2024-07-26 14:24:43 +0545

    new changes

---
+++ src/pages/PriceList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PriceList = () => {
+  return (
+     <>
+     
+     </>
+  )
+}
+
+export default PriceList
---
+++ src/pages/Vehicle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Vehicle = () => {
+  return (
+     <>
+     
+     </>
+  )
+}
+
+export default Vehicle
