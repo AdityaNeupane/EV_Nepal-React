83689fa40907ef8b90181aab4ddc8717da13220e
Suraz Subedi <surazsubedi28@gmail.com>
2024-07-28 15:05:51 +0545

    tag update

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,24 +1,39 @@
-import React from 'react'
-import './Info.css'
+import React from "react";
+import "./Info.css";
 const Info = () => {
   return (
-     <>
-     <div className='info-section'>
+    <>
+      <div className="info-section">
         <h1>Why Electric Vehicles are the Future of Transportation?</h1>
         <br></br>
         <br></br>
-        <p1>
-            Electric vehicles are the future of transportation due to their environmental, economic, and technological benefits. EVs produce zero tailpipe emissions, significantly reducing air pollution and greenhouse gases. With renewable energy sources, their lifecycle becomes even more sustainable. Economically, EVs are cheaper to operate and maintain. Technological advancements have improved battery efficiency, range, and charging times. Government incentives and investments in charging infrastructure are accelerating adoption. As we prioritize environmental issues and benefit from technological advancements, EVs offer a cleaner, more efficient, and sustainable transportation future.
-       </p1>
-       <br></br>
-       <br></br>
-       <p2>
-        Electric vehicles are becoming increasingly popular in Nepal too, reflecting a global shift towards sustainable transportation. In the first half of the fiscal year 2023-24, EV imports accounted for a significant portion of the country's auto import value, thanks to their cost-effectiveness and environmental benefits. Financial institutions in Nepal are also supporting this trend by offering loans that cover up to 90% of the total cost of EV's.
-       </p2>
-     </div>
-     
-     </>
-  )
-}
+        <p>
+          Electric vehicles are the future of transportation due to their
+          environmental, economic, and technological benefits. EVs produce zero
+          tailpipe emissions, significantly reducing air pollution and
+          greenhouse gases. With renewable energy sources, their lifecycle
+          becomes even more sustainable. Economically, EVs are cheaper to
+          operate and maintain. Technological advancements have improved battery
+          efficiency, range, and charging times. Government incentives and
+          investments in charging infrastructure are accelerating adoption. As
+          we prioritize environmental issues and benefit from technological
+          advancements, EVs offer a cleaner, more efficient, and sustainable
+          transportation future.
+        </p>
+        <br></br>
+        <br></br>
+        <p>
+          Electric vehicles are becoming increasingly popular in Nepal too,
+          reflecting a global shift towards sustainable transportation. In the
+          first half of the fiscal year 2023-24, EV imports accounted for a
+          significant portion of the country's auto import value, thanks to
+          their cost-effectiveness and environmental benefits. Financial
+          institutions in Nepal are also supporting this trend by offering loans
+          that cover up to 90% of the total cost of EV's.
+        </p>
+      </div>
+    </>
+  );
+};
 
-export default Info
+export default Info;
