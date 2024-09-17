2ca504c9b5d7884518093928b1841f98f81704c3
adityaneupane <bijayneupane324@gmail.com>
2024-09-17 08:47:54 +0545

    changes

--- src/aboutUs/Aboutcompany.jsx
+++ src/aboutUs/Aboutcompany.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import './Aboutcompany.css'
+import illustration5 from '../assets/illustrations/illustration5.png';
 const Aboutcompany = () => {
   return (
      <>
       
      <div className='about-company'>
 
-      <div className='company'>
-
-      <div className='left-box'>
-        <h1 className='left-box-content'>About <br></br>EV NEPAL</h1>
+      <div className='about-leftbox'>
+        <img src={illustration5} alt="illustration5" />
       </div>
-
-      <div className='right-box'>
-      <h1 className='right-box-content'> Established in 2024, EV Nepal is your ultimate destination for everything related to electric vehicles. We are committed to providing comprehensive information on EV prices, various types of available and up-coming electric vehicles, nearby charging stations, and many more. As a one-stop destination for EV, our goal is to empower consumers and promote the adoption of eco-friendly transportation across the country. At EV Nepal, we strive to make the transition to electric mobility as seamless and informed as possible.    </h1>
-      </div>
-
+    
+      <div className='about-rightbox'>
+        <div className='about-upperrightbox'>
+          <h1 className='about-upperrightboxheading'>About EV Nepal</h1>
         </div>
-     
-
+        <div className='about-lowerrightbox'>
+          <h1 className='about-lowerrightboxheading'>Established in 2024, EV Nepal is your ultimate destination for everything related to electric vehicles. We are committed to providing comprehensive information on EV prices, various types of available and up-coming electric vehicles, nearby charging stations, and many more. As a one-stop destination for EV, our goal is to empower consumers and promote the adoption of eco-friendly transportation across the country. At EV Nepal, we strive to make the transition to electric mobility as seamless and informed as possible. </h1>
+        </div>
       </div>
-     
+
+     </div>
      </>
   )
 }
